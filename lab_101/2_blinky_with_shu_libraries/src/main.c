71001e10aa2f697cd5c1fc26f3c6763daf37c926
Callum <cal_b22_is@hotmail.com>
2017-09-28 12:31:04 +0100

    Added Lighting Sequence

--- lab_101/2_blinky_with_shu_libraries/src/main.c
+++ lab_101/2_blinky_with_shu_libraries/src/main.c
@@ -19,8 +19,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PI1 (again, this is the inbuilt led)
-gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
-gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};		//Red LED
+gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};	//Amber LED
+gpio_pin_t led3 = {PB_15, GPIOB, GPIO_PIN_15};	//Green LED
 
 // this is the main method
 int main()
@@ -33,20 +34,43 @@ int main()
   // initialise the gpio pins
   init_gpio(led1, OUTPUT);
   init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
   
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
+    // red on
     toggle_gpio(led1);
     
-    // wait for 1 second
-    HAL_Delay(1000);
+    // wait for 4 second
+    HAL_Delay(4000);
 		
-    // toggle the led on the gpio pin
+		// amber on
+		toggle_gpio(led2);
+		
+		//delay 1 second
+		HAL_Delay(1000);
+			
+		//red off
+		toggle_gpio(led1);
+		//amber off		
     toggle_gpio(led2);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+    //Green on
+		toggle_gpio(led3);
+		
+    // wait for 3 second
+    HAL_Delay(3000);
+		
+		//amber on		
+    toggle_gpio(led2);
+    //Green off
+		toggle_gpio(led3);
+		
+		//delay 1 second
+		HAL_Delay(1000);
+		
+		//amber off		
+    toggle_gpio(led2);
+		
   }
 }
