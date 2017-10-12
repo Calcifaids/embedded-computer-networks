01e6635e077f8c78fcae4ce61746373dcee595b8
Callum <cal_b22_is@hotmail.com>
2017-10-12 12:01:19 +0000

    Added LED's & Bar

--- lab_102/1_lcd_example/src/main.c
+++ lab_102/1_lcd_example/src/main.c
@@ -15,6 +15,8 @@
 #include "pinmappings.h"
 #include "clock.h"
 #include "stm32746g_discovery_lcd.h"
+#include "adc.h"
+#include "gpio.h"
 
 // LCD DEFINES
 
@@ -28,8 +30,20 @@ const char * welcome_message[2] =
   "*      Welcome to SHU      *"
 };
 
+void clearLeds(void);
+
+
+gpio_pin_t pot = {PA_0, GPIOA, GPIO_PIN_0};
+gpio_pin_t redLed = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t orangeLed = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t yellowLed = {PB_15, GPIOB, GPIO_PIN_15};
+gpio_pin_t greenLed = {PA_8, GPIOA, GPIO_PIN_8};
+
 // CODE
 
+char led[20];
+char barBuffer[27];
+	
 // this is the main method
 int main()
 {
@@ -37,42 +51,90 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
-  
+	
+	//Setup pot & LED's
+	init_adc(pot);
+	init_gpio(redLed, OUTPUT);
+	init_gpio(orangeLed, OUTPUT);
+	init_gpio(yellowLed, OUTPUT);
+	init_gpio(greenLed, OUTPUT);
+	
   // initialise the lcd
   BSP_LCD_Init();
   BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
   BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
 
   // set the background colour to blue and clear the lcd
-  BSP_LCD_SetBackColor(LCD_COLOR_BLUE);
-  BSP_LCD_Clear(LCD_COLOR_BLUE);
+  BSP_LCD_SetBackColor(LCD_COLOR_BROWN);
+  BSP_LCD_Clear(LCD_COLOR_BROWN);
   
   // set the font to use
   BSP_LCD_SetFont(&Font24); 
   
   // print the welcome message ...
-  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
   BSP_LCD_DisplayStringAtLine(0, (uint8_t *)BOARDER);
   BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_message[0]);
   BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_message[1]);
   BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER); 
     
   // delay a little ...
-  HAL_Delay(5000);
+  HAL_Delay(2000);
   
-  // display an "uptime" counter
-  BSP_LCD_DisplayStringAtLine(5, (uint8_t *)"Current uptime =");
-  int counter = 0;
   while(1)
   {
+		//Get ADC and convert (Fix this with calibratoin).
+		uint16_t adc_val = read_adc(pot);
+		adc_val = (adc_val * 100) / 4095.0;
+		
+		//switch LED
+		if (adc_val <= 24){
+			clearLeds();
+			write_gpio(redLed, HIGH);
+			char val[6] = "red";
+			sprintf (led, "LED on = %s", val); 
+		}
+		else if((adc_val > 24) && (adc_val <= 49)){
+			clearLeds();
+			write_gpio(orangeLed, HIGH);
+			char val[6] = "orange";
+			sprintf (led, "LED on = %s", val); 
+		}
+		else if(adc_val > 49 && adc_val <= 74){
+			clearLeds();
+			write_gpio(yellowLed, HIGH);
+			char val[6] = "yellow";
+			sprintf (led, "LED on = %s", val); 
+		}
+		else{
+			clearLeds();
+			write_gpio(greenLed, HIGH);
+			char val[6] = "green";
+			sprintf (led, "LED on = %s", val); 
+		}
+		
     // format a string based around the uptime counter
-    char str[20];
-    sprintf(str, "%d s", counter++);
-    
+    char str[12];
+    sprintf(str, "ADC = %d%%", adc_val);
     // print the message to the lcd
+    BSP_LCD_ClearStringLine(5);
+    BSP_LCD_DisplayStringAtLine(5, (uint8_t *)str);
     BSP_LCD_ClearStringLine(6);
-    BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
-    
+		BSP_LCD_DisplayStringAtLine(6, (uint8_t *)led);
+		char barBuffer[27] = {0};
+		adc_val = (adc_val * 29) / 100.0;
+		for (int i = 0; i < adc_val; i ++){
+			barBuffer[i] = '#';
+		}
+		BSP_LCD_ClearStringLine(7);
+		BSP_LCD_DisplayStringAtLine(7, (uint8_t *)barBuffer);
     HAL_Delay(1000);
   }
+}
+
+void clearLeds(){
+	write_gpio(redLed, LOW);
+	write_gpio(orangeLed, LOW);
+	write_gpio(yellowLed, LOW);
+	write_gpio(greenLed, LOW);
 }
